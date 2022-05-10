8002ab3e38991e0bd15fb1f31e5b7d19521a5ae4
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-05-09 21:31:52 -0400

    Remove harvest event as we handle it in the block.

--- src/main/java/com/pam/pamhc2trees/events/EventSetup.java
+++ src/main/java/com/pam/pamhc2trees/events/EventSetup.java
@@ -1,14 +1,10 @@
 package com.pam.pamhc2trees.events;
 
-import com.pam.pamhc2trees.events.harvest.FruitHarvest;
-
 import net.minecraftforge.common.MinecraftForge;
-
 
 public class EventSetup {
 
 	public static void setupEvents() {
 		MinecraftForge.EVENT_BUS.register(new TemptationTask());
-		MinecraftForge.EVENT_BUS.register(new FruitHarvest());
 	}
 }
